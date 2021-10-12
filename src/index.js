5f01cfffe6b39b19635af381c412989ddbd5a81e
Pooja-bhaumik <pbhaumik200@gmail.com>
2021-10-12 17:50:38 +0530

    understand the redux in more depth

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import allReducers from "./reducers";
+import ReactDOM from "react-dom";
+
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./App";
+
+// const reducer = (state = 0, action) => {
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return state + action.payload;
+//     case "DECRIEMENT":
+//       return state - action.payload;
+//     default:
+//       return state;
+//   }
+// };
+// const store = createStore(reducer);
+// store.subscribe(() => {
+//   console.log("current state", store.getState());
+// });
+// store.dispatch({
+//   type: "INCREMENT",
+//   payload: 1,
+// });
+// store.dispatch({
+//   type: "DECRIEMENT",
+//   payload: 2,
+// });
+
+// store.dispatch({
+//   type: "INCREMENT",
+//   payload: 3,
+// });
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/reducers/index.jsx
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import userReducer from "./user";
+import counterReducer from "./counter";
+const allReducers = combineReducers({
+  userReducer,
+  counterReducer,
+});
+export default allReducers;
